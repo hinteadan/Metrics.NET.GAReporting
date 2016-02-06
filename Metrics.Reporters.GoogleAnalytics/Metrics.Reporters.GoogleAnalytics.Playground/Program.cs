0a1696042838554d1af123da92c98ab3275934e0
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2016-02-07 00:46:40 +0200

    Scaffold wireup

---
+++ Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics.Playground/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metrics.Reporters.GoogleAnalytics.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metric.Config
+                .WithAllCounters()
+                .WithInternalMetrics()
+                .WithReporting(cfg => cfg.WithGoogleAnalytics());
+
+            Console.WriteLine("Done @ {0}", DateTime.Now);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics/GoogleAnalyticsConfigExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Metrics.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metrics.Reporters.GoogleAnalytics
+{
+    public static class GoogleAnalyticsConfigExtensions
+    {
+        public static MetricsReports WithGoogleAnalytics(this MetricsReports reports)
+        {
+            return reports.WithReport(new GoogleAnalyticsReport(), TimeSpan.FromSeconds(5));
+        }
+    }
+}
