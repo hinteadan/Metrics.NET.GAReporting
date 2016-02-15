8cd4dbadd65157ea8310363c70e2991255f41eea
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2016-02-15 16:12:50 +0200

    Prepare HTTP tracking call data

--- Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics.Tracker/Model/MeasurementProtocol/Protocol.cs
+++ Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics.Tracker/Model/MeasurementProtocol/Protocol.cs
@@ -12,7 +12,9 @@ namespace Metrics.Reporters.GoogleAnalytics.Tracker.Model.MeasurementProtocol
         protected const string googleHttpUrl = "http://www.google-analytics.com/collect";
         protected const string googleHttpsUrl = "https://ssl.google-analytics.com/collect";
 
-        private readonly string baseUrl;
+        private const string userAgent = "Metrics.NET/2.0 (Windows NT 10.0; Win64; x64; rv:2.0) Metrics.NET.GoogleAnalytics.Reporter/1 GoogleAnalytics.Tracker/1.0";
+
+        private readonly Uri baseUrl;
         protected readonly Parameter version = Parameter.Text(ParameterName.Version, ProtocolVersionValue.Version);
         protected readonly Parameter trackingId;
         protected readonly Parameter clientId;
@@ -21,7 +23,7 @@ namespace Metrics.Reporters.GoogleAnalytics.Tracker.Model.MeasurementProtocol
 
         protected Protocol(string url, string trackingId, string clientId)
         {
-            this.baseUrl = url;
+            this.baseUrl = new Uri(url);
             this.trackingId = Parameter.Text(ParameterName.TrackingId, new TrackingIdValue(trackingId));
             this.clientId = Parameter.Text(ParameterName.ClientId, new ParameterTextValue(clientId, ASCIIEncoding.Unicode.GetByteCount(clientId)));
 
@@ -43,6 +45,28 @@ namespace Metrics.Reporters.GoogleAnalytics.Tracker.Model.MeasurementProtocol
         {
             this.WithParameter(Parameter.Text(ParameterName.HitType, hitType));
             return this;
+        }
+
+        public Uri Url
+        {
+            get
+            {
+                return baseUrl;
+            }
+        }
+
+        public string UserAgent
+        {
+            get
+            {
+                return userAgent;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return string.Join("&", this.parameters.Values);
         }
     }
 }
