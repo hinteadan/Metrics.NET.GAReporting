5584daba7569755184748e316738cf705a475307
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2016-02-16 15:04:10 +0200

    Play around with reporting

--- Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics.Playground/Program.cs
+++ Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics.Playground/Program.cs
@@ -16,6 +16,14 @@ namespace Metrics.Reporters.GoogleAnalytics.Playground
                 .WithInternalMetrics()
                 .WithReporting(cfg => cfg.WithGoogleAnalytics());
 
+            var counter1 = Metric.Counter("Some Counter", Unit.Calls);
+            var counter2 = Metric.Counter("Some Other Counter", Unit.Events);
+            var counter3 = Metric.Counter("Another Counter", Unit.Items);
+
+            counter1.Increment();
+            counter2.Increment();
+            counter3.Increment();
+
             Console.WriteLine("Done @ {0}", DateTime.Now);
             Console.ReadKey();
         }
