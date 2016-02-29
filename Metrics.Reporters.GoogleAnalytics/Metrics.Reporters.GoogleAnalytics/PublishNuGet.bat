5886c687ece535164c269e821fda9f59627b1248
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2016-02-29 22:23:47 +0200

    Nuget pack config

---
+++ Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics/PublishNuGet.bat
@@ -0,0 +1,7 @@
+﻿del *.nupkg
+
+nuget.exe pack .\Metrics.Reporters.GoogleAnalytics.csproj -Prop Configuration=Release -IncludeReferencedProjects
+
+nuget.exe setApiKey 79ccf130-386b-41b8-a8e2-6ce17f0459ca
+
+nuget.exe push ".\*.nupkg"
