0a6e053959a8dca9329730995a8d1f0c0bb87a19
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2016-03-07 16:00:57 +0200

    Inlcude ref. projects

--- Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics/PublishNuGet.bat
+++ Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics/PublishNuGet.bat
@@ -1,6 +1,6 @@
 ﻿del *.nupkg
 
-nuget.exe pack .\Metrics.Reporters.GoogleAnalytics.csproj -Prop Configuration=Release
+nuget.exe pack .\Metrics.Reporters.GoogleAnalytics.csproj -Prop Configuration=Release -IncludeReferencedProjects
 
 nuget.exe setApiKey 79ccf130-386b-41b8-a8e2-6ce17f0459ca
 
