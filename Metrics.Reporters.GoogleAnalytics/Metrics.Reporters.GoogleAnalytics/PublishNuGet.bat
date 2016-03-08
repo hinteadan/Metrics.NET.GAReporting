d6edb0d4bc1281efe31e420c50345eb064e9819d
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2016-03-08 14:55:06 +0200

    Fix PublishNuGet.bat

--- Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics/PublishNuGet.bat
+++ Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics/PublishNuGet.bat
@@ -1,4 +1,4 @@
-del *.nupkg
+del /S /Q .\*nupkg
 nuget.exe pack .\Metrics.Reporters.GoogleAnalytics.csproj -Prop Configuration=Release -IncludeReferencedProjects
 nuget.exe setApiKey 79ccf130-386b-41b8-a8e2-6ce17f0459ca
 nuget.exe push ".\*.nupkg"
