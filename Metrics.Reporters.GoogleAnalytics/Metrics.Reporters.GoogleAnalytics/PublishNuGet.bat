39b034e542b7de517fa023641068100f5adb75ca
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2016-03-08 14:44:55 +0200

    Correct whitespaces in PublishNuGet.bat

--- Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics/PublishNuGet.bat
+++ Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics/PublishNuGet.bat
@@ -1,7 +1,4 @@
-﻿del *.nupkg
-
+del *.nupkg
 nuget.exe pack .\Metrics.Reporters.GoogleAnalytics.csproj -Prop Configuration=Release -IncludeReferencedProjects
-
 nuget.exe setApiKey 79ccf130-386b-41b8-a8e2-6ce17f0459ca
-
 nuget.exe push ".\*.nupkg"
