82c1a6e097e4fb19fa000956d71aff621cf06fc5
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2016-03-17 11:19:34 +0200

    Update publish script to build the solution prior to nuget push

--- Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics/PublishNuGet.bat
+++ Metrics.Reporters.GoogleAnalytics/Metrics.Reporters.GoogleAnalytics/PublishNuGet.bat
@@ -1,3 +1,4 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe ".\Metrics.Reporters.GoogleAnalytics.csproj" /t:Build /p:Configuration=Release
 del /S /Q .\*nupkg
 nuget.exe pack .\Metrics.Reporters.GoogleAnalytics.csproj -Prop Configuration=Release -IncludeReferencedProjects
 nuget.exe setApiKey 79ccf130-386b-41b8-a8e2-6ce17f0459ca
